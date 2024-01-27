66e2b578da9dae951baf1a8d96a4d134dab7b5d6
Tomasz Czerniewski <brydlo2.tc@gmail.com>
2024-01-27 22:26:51 +0100

    new class with implementation StatisticalCalculator

---
+++ Learning Progress Tracker (Java)/task/src/tracker/statistics/StatisticalCalculator.java
@@ -0,0 +1,27 @@
+package tracker.statistics;
+
+import tracker.Points;
+
+import java.util.Map;
+
+public class StatisticalCalculator implements StatisticalAnalysis {
+    @Override
+    public String averageCompletionPerStudent(long id, Map<Long, Points> pointsMap) {
+        return null;
+    }
+
+    @Override
+    public String coursePopularity(long[] arrayWithPoints) {
+        return null;
+    }
+
+    @Override
+    public String courseActivity(long[] arrayWithPoints) {
+        return null;
+    }
+
+    @Override
+    public long StudentsRanking(Map<Long, Points> pointsMap) {
+        return 0;
+    }
+}
